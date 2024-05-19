4eb571c6827a9465e719002328647725f47be0dd
Kartik Kochhar <kochharkartik51@gmail.com>
2024-05-19 12:32:01 +0530

    purged outputs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  
 }
